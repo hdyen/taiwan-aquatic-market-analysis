5b19d380a06db4669ffad61d6f5673ae77a9c9a2
hdyen <hongda.yen@gmail.com>
2017-11-10 01:21:18 +0800

    initial commit

--- crawler/json_load.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('aquatic_trans_data.json') as f:
-    aquatic_trans_data = json.load(f)[0]
-    markets = aquatic_trans_data['markets'].items()
-    fish_types = aquatic_trans_data['fish_types'].items()
-
-
