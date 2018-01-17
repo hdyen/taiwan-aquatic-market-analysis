3b58cb353b6fbf6f15a7bdaac7beaad14513d23e
hdyen <hongda.yen@gmail.com>
2018-01-18 07:56:23 +0800

    initial crawl main driver program

---
+++ crawl.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.6
+
+"""A simple Taiwan aquatic market crawler -- main driver program."""
+
+import argparse
+import crawler.crawling
+
+ARGS = argparse.ArgumentParser(description='Taiwan aquatic market crawler')
+ARGS.add_argument()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
